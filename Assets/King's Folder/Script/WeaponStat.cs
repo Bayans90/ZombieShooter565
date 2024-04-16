8fd7ab1df04f246e51dac0f3bab915f8a43cc59c
kingbaysic <146765417+kingbaysic@users.noreply.github.com>
2024-04-15 17:00:15 -0700

    w

---
+++ Assets/King's Folder/Script/WeaponStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStat : MonoBehaviour
+{
+    public int weaponType; //1 melee, 2 gun
+    public bool isAuto_gun;
+    public int attackPoints;
+
+}
