11fb4b594c8b6d8a8da5e4a66326d7c9c0d2a2ac
Bayans90 <bayan.saab.577@my.csun.edu>
2024-05-02 20:45:31 -0700

    fixed zombie spawner

--- Assets/Bayan's Folder/Scripts/ZombieSpawnController.cs
+++ Assets/Bayan's Folder/Scripts/ZombieSpawnController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ZombieSpawnController : MonoBehaviour
 {
-    public int initialZombiePW = 3;
-    public int currentZombiePW;
+    public int initialZombiePW = 2;
+    public double currentZombiePW;
 
     public float spawnDelay = 3.0f; //Delay of time between zombie spawns
     public int currWave = 0;
@@ -15,9 +15,12 @@ public class ZombieSpawnController : MonoBehaviour
     public float cooldownCounter = 0;
 
     public List <ZombieController> currZombAlive;
+    public List<ZombieController> removeZomb;
 
     public GameObject zombiePrefab;
     public Transform target;
+
+    
 
     private void Start()
     {
@@ -35,6 +38,8 @@ public class ZombieSpawnController : MonoBehaviour
 
     private IEnumerator SpawnWave()
     {
+        
+
         for(int i = 0; i < currentZombiePW;++i)
         {
             //Offset from spawner to give spawn variety
@@ -43,11 +48,13 @@ public class ZombieSpawnController : MonoBehaviour
 
             //Create the zombie
             var zombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
-            zombie.gameObject.GetComponentInChildren<ZombieController>().target = target;
-            //Retrieve Zombie Script 
-            ZombieController zombieScript = zombie.GetComponent<ZombieController>();
 
-            //TrackZombie
+            zombie.gameObject.GetComponentInChildren<ZombieController>().target = target;
+
+            //Retrieve Zombie Script 
+            ZombieController zombieScript = zombie.GetComponentInChildren<ZombieController>();
+
+            //Track Zombie
             currZombAlive.Add(zombieScript);
 
             yield return new WaitForSeconds(spawnDelay);
@@ -55,14 +62,21 @@ public class ZombieSpawnController : MonoBehaviour
         }
     }
 
+    
+
+
     private void Update()
     {
-        List<ZombieController> removeZomb = new List<ZombieController> ();
-        foreach (ZombieController zombie in removeZomb)
+        removeZomb = new List<ZombieController>();
+        
+
+        foreach (ZombieController zombie in currZombAlive)
         {
             if (zombie.isDead)
             {
                 removeZomb.Add(zombie);
+                Destroy(zombie.gameObject, 5);
+
             }
         }
 
@@ -71,7 +85,7 @@ public class ZombieSpawnController : MonoBehaviour
             currZombAlive.Remove(zombie);
         }
 
-        removeZomb.Clear();
+        //removeZomb.Clear();
 
         if(currZombAlive.Count == 0 && inCooldown == false)
         {
@@ -87,7 +101,7 @@ public class ZombieSpawnController : MonoBehaviour
 
         inCooldown = false;
 
-        currentZombiePW *= 2;
+        currentZombiePW *= 1.25;
         StartNextWave();
     }
 }
